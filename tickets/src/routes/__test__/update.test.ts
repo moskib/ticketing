ac195168dd4f300c92f1b6edb2618494b354cc98
moskib <moshekiba3@gmail.com>
2024-06-20 08:14:36 +0300

    fix tests for update + create ticket

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -97,4 +98,26 @@ it('updates the ticket provided valid inputs', async () => {
     .send();
   expect(ticketResponse.body.title).toEqual('updated ticket');
   expect(ticketResponse.body.price).toEqual(200);
+});
+
+it('publishes an event', async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'asdfasdf',
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'updated ticket',
+      price: 200,
+    })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
