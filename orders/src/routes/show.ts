c299682895a60a67086fb1a5b240e37105d01ab5
moskib <moshekiba3@gmail.com>
2024-06-27 08:27:40 +0300

    show route tests

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,31 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@mkgittix/core';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+import { param } from 'express-validator';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  [param('orderId').isMongoId()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
