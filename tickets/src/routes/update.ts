4c57c24a22d002d56eaf7e4643a4a9c97fa5aae8
moskib <moshekiba3@gmail.com>
2024-07-08 08:57:52 +0300

    reject ticket update if the ticket is reserved

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,6 +5,7 @@ import {
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
+  BadRequestError,
 } from '@mkgittix/core';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-update-publisher';
@@ -28,6 +29,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket');
     }
 
     if (ticket.userId !== req.currentUser!.id) {
