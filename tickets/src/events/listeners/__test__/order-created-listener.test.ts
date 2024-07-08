3a6db685070dd2ed75122121713fdf194b014f21
moskib <moshekiba3@gmail.com>
2024-07-08 08:22:18 +0300

    publish ticket updated event

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -47,10 +47,25 @@ it('sets the userId of the ticket', async () => {
 
   expect(updatedTicket!.orderId).toEqual(data.id);
 });
+
 it('acks the the message', async () => {
   const { data, listener, msg, ticket } = await setup();
 
   await listener.onMessage(data, msg);
 
   expect(msg.ack).toHaveBeenCalled();
+});
+
+it('publishes a ticket updated event', async () => {
+  const { data, listener, msg, ticket } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const ticketUpdatedData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(data.id).toEqual(ticketUpdatedData.orderId);
 });
--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,8 @@
-import {
-  Listener,
-  OrderCreatedEvent,
-  OrderStatus,
-  Subjects,
-} from '@mkgittix/core';
+import { Listener, OrderCreatedEvent, Subjects } from '@mkgittix/core';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-update-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -26,6 +22,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // Mark the ticket as being reserved by setting its orderId property
     ticket.set({ orderId: data.id });
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
 
     //Save the ticket
     await ticket.save();
