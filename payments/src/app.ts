f4bbfee4548ef57905911e55e7698bdbb3c7def7
moskib <moshekiba3@gmail.com>
2024-07-14 08:36:42 +0300

    payments infra

---
+++ payments/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { NotFoundError, currentUser, errorHandler } from '@mkgittix/core';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // whether the cookie should be encrypted
+    secure: process.env.NODE_ENV !== 'test', // only visit if we have https connection
+  })
+);
+app.use(currentUser);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
