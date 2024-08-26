a2f40ef2b9411856b1edfc2a6f58d53a5001e440
moskib <moshekiba3@gmail.com>
2024-08-26 08:25:02 +0300

    remove nats test

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from './base-publisher';
-import { Subjects } from './subjects';
-import { TicketCreatedEvent } from './ticket-created-event';
-
-export class TicketcreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-}
